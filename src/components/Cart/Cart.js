9082a30e617b92db5d1fa3b51e1e202c3b7baa90
Md Mehedi Ahsan <ahsanmehedi041@gmail.com>
2022-09-29 01:29:54 +0600

    add cart break time

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,10 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Cart = ({cart}) => {
+    const [restTime, setRestTime] = useState(0);
+    console.log(restTime);
     let time = 0;
     for(const activity of cart){
         time = time + activity.time;
     }
+
+    const breakTime = [10,20,30,40,50];
+
     return (
         <div>
             <div className='flex'>
@@ -16,10 +21,16 @@ const Cart = ({cart}) => {
                     <p>height</p>
                     <p>Age</p>
                 </div>
-                <h1 className='font-semibold text-xl'>Add A Break</h1>
+                <div>
+                    <h1 className='font-semibold text-xl'>Add A Break</h1>
+                    {
+                        breakTime.map(bt => <button onClick={() => setRestTime(bt)} className="bg-info text-white p-2 rounded-full mr-1">{bt}s</button>)
+                    }
+                </div>
                 <div>
                     <h1 className='font-semibold text-xl'>Exercise Details</h1>
                     <p>Exercise time: {time} seconds</p>
+                    <p>Break time: {restTime} seconds</p>
                 </div>
         </div>
     );
